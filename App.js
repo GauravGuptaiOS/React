4e2d88003028dd30161ff82460daa1365b58e3fd
gaurav.gupta@appinventiv.com <Gaurav@123>
2025-11-22 18:46:37 +0530

    Episode 02

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
 const heading = React.createElement("div", {
     id : "heading"
-}, [React.createElement("div",{id : "child1"}, 
-     [React.createElement("h1",{},"I am h1 tag"),
-      React.createElement("h2",{},"I am h2 tag")
+}, [React.createElement("div",{id : "child1", key : "child1"}, 
+     [React.createElement("h1",{key : "h1"},"I am h1 tag"),
+      React.createElement("h2",{key : "h2"},"I am h2 tag")
     ]),
-    React.createElement("div",{id : "child1"}, 
-     [React.createElement("h1",{},"I am h1 tag"),
-      React.createElement("h2",{},"I am h2 tag")
+    React.createElement("div",{id : "child2", key : "child2" }, 
+     [React.createElement("h1",{key : "h3"},"I am h1 tag"),
+      React.createElement("h2",{key : "h4"},"I am h2 tag")
     ])
 ]
 )
 const root = ReactDOM.createRoot(document.getElementById("root"))
+
 root.render(heading)
