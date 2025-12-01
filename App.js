048be759287637aba66b699e7e6470a99f0abc8a
gaurav.gupta@appinventiv.com <Gaurav@123>
2025-12-01 12:21:01 +0530

    Episode 03

--- App.js
+++ App.js
@@ -1,18 +1,44 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const heading = React.createElement("div", {
-    id : "heading"
-}, [React.createElement("div",{id : "child1", key : "child1"}, 
-     [React.createElement("h1",{key : "h1"},"I am h1 tag"),
-      React.createElement("h2",{key : "h2"},"I am h2 tag")
-    ]),
-    React.createElement("div",{id : "child2", key : "child2" }, 
-     [React.createElement("h1",{key : "h3"},"I am h1 tag"),
-      React.createElement("h2",{key : "h4"},"I am h2 tag")
-    ])
-]
-)
+//ReactElement => JS Object => JS object into a real DOM element
+const heading = React.createElement("h1", {id : "heading"}, "This is React")
 const root = ReactDOM.createRoot(document.getElementById("root"))
+//root.render(heading)
 
-root.render(heading)
+// JSX => ReactElement => JS Object => HTML Element(render)
+
+// React Element
+const jsxHeading = (
+<h1> This is heading using JSX </h1>
+)
+
+
+//Functional Component : A JS function that return some piece of JSX or react element is called functional component
+const HeadingComponent = () => <h1> This is react element using functional component</h1>
+
+const HeadingComponent2 = () => {
+  return <h1> Heading component with return  </h1>
+}
+
+const HeadingComponent3 = () => (
+  <h1>This is functional component without explicit return </h1>
+)
+
+//Compositional Component
+const HeadingComponent4 = () => (
+  <div id = "container" >
+  <HeadingComponent3/>
+  <h2> This is compositional component</h2>
+  </div>
+)
+
+const title = (
+  <div>
+     {jsxHeading}
+     <h1> This is React Element </h1>
+     <h2> This is React Element </h2>
+  </div>
+  
+) 
+root.render(title )
